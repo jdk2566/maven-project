9b36900647d11ea899192599f47f2a376d3e0917
Chen <1547794076@qq.com>
2023-04-09 16:13:06 +0800

    第一次修改

--- src/main/java/com/itheima/HelloWorld.java
+++ src/main/java/com/itheima/HelloWorld.java
@@ -3,5 +3,9 @@ package com.itheima;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Maven~");
+        System.out.println("Hello Maven~");
+        System.out.println("Hello Maven~");
+        System.out.println("Hello Maven~");
+        System.out.println("Hello Maven~");
     }
 }
